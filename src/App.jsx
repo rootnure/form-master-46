42b4b8b528b70975201caaa5bcdf7cbde664774f
rootnure <nur.diu.2791@gmail.com>
2023-09-20 20:45:09 +0600

    Basic Setup & Simple way to handle form

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import SimpleForm from './components/SimpleForm/SimpleForm'
+
+function App() {
+
+  return (
+    <>
+      
+      <h1>Vite + React</h1>
+      
+      <SimpleForm></SimpleForm>
+    </>
+  )
+}
+
+export default App
---
+++ src/__form__.js
@@ -0,0 +1,5 @@
+/**
+ * 1. use form tag and onSubmit handler with event (e)
+ * 1.1. From e, access value like this: e.target.[name_of_the_input_field].value
+ * 
+ */
